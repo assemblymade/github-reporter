42b4501ff28ef57fd91a7fb5609840f85a69f3db
barisser <barisser@gmail.com>
2015-05-06 12:35:54 -0700

    controllers render message

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -1,8 +1,13 @@
 class ActivityController < ApplicationController
-  respond_to :json
+  respond_to :json, :html
 
   def index
     puts "RECEIVED MESSAGE"
     puts params
   end
+
+  def welcome
+    render json: {:message => "hi there"}
+  end
+
 end
