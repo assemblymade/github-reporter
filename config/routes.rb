3c8f3c1a50a58dfba7b33327b00172e5f988f1a2
barisser <barisser@gmail.com>
2015-05-06 12:44:05 -0700

    setup db

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   post '/activity' => 'activity#index'
-
+  get '/welcome' => 'activity#welcome'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
